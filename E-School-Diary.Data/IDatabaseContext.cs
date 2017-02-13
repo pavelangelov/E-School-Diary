0c17f4c20ec8eab3bf486e25f7be48121ec13de4
pavelangelov <p_a_v_el@abv.bg>
2017-02-13 22:41:35 +0200

    Added EF-Database Frist

--- E-School-Diary.Data/IDatabaseContext.cs
+++ E-School-Diary.Data/IDatabaseContext.cs
@@ -1,9 +1,21 @@
-﻿using System.Data.Entity;
+﻿using E_School_Diary.Data.EF_DataSource;
+using System.Data.Entity;
 
-namespace E_School_Diary.Data.DB.Contracts
+namespace E_School_Diary.Data
 {
     public interface IDatabaseContext
     {
+        DbSet<C__MigrationHistory> C__MigrationHistory { get; set; }
+        DbSet<AspNetRole> AspNetRoles { get; set; }
+        DbSet<AspNetUserClaim> AspNetUserClaims { get; set; }
+        DbSet<AspNetUserLogin> AspNetUserLogins { get; set; }
+        DbSet<AspNetUser> AspNetUsers { get; set; }
+        DbSet<Lecture> Lectures { get; set; }
+        DbSet<Mark> Marks { get; set; }
+        DbSet<Message> Messages { get; set; }
+        DbSet<StudentClass> StudentClasses { get; set; }
+        DbSet<User> Users { get; set; }
+
         int Save();
     }
 }
