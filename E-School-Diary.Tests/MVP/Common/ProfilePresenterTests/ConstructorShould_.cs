ca49254971edb18c0a2cf29d970d2b3c5faeb429
pavelangelov <p_a_v_el@abv.bg>
2017-02-21 23:08:08 +0200

    Added unit tests for ProfilePresenter

---
+++ E-School-Diary.Tests/MVP/Common/ProfilePresenterTests/ConstructorShould_.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+using Moq;
+using NUnit.Framework;
+
+using E_School_Diary.Services.Contracts;
+using E_School_Diary.WebClient.Presenters.Common;
+using E_School_Diary.WebClient.Views.Common;
+
+namespace E_School_Diary.Tests.MVP.Common.ProfilePresenterTests
+{
+    class ConstructorShould_
+    {
+        [Test]
+        public void ThrowArgumentNullException_IfUserServiceIsNull()
+        {
+            // Arrange
+            var mockedView = new Mock<IProfileView>();
+            var mockedImageService = new Mock<IImageUploadService>();
+
+            // Act & Assert
+            var message = Assert.Throws<ArgumentNullException>(() => new ProfilePresenter(mockedView.Object, null, mockedImageService.Object)).Message;
+            Assert.IsTrue(message.Contains("userService"));
+        }
+
+        [Test]
+        public void ThrowArgumentNullException_IfImageUploadServiceIsNull()
+        {
+            // Arrange
+            var mockedView = new Mock<IProfileView>();
+            var mockedUserService = new Mock<IUserService>();
+
+            // Act & Assert
+            var message = Assert.Throws<ArgumentNullException>(() => new ProfilePresenter(mockedView.Object, mockedUserService.Object, null)).Message;
+            Assert.IsTrue(message.Contains("imageUploadService"));
+        }
+
+        [Test]
+        public void NotToThrow_IfPassedParametersAreValid()
+        {
+            // Arrange
+            var mockedView = new Mock<IProfileView>();
+            var mockedUserService = new Mock<IUserService>();
+            var mockedImageService = new Mock<IImageUploadService>();
+
+            // Act & Assert
+            Assert.DoesNotThrow(() => new ProfilePresenter(mockedView.Object, mockedUserService.Object, mockedImageService.Object));
+        }
+    }
+}
---
+++ E-School-Diary.Tests/MVP/Common/ProfilePresenterTests/View_PageLoadShould_.cs
@@ -0,0 +1,40 @@
+﻿using Moq;
+using NUnit.Framework;
+
+using E_School_Diary.Services.Contracts;
+using E_School_Diary.Utils.DTOs.Common;
+using E_School_Diary.WebClient.Models.CustomEventArgs;
+using E_School_Diary.WebClient.Models.ViewModels.Common;
+using E_School_Diary.WebClient.Presenters.Common;
+using E_School_Diary.WebClient.Views.Common;
+
+namespace E_School_Diary.Tests.MVP.Common.ProfilePresenterTests
+{
+    [TestFixture]
+    public class View_PageLoadShould_
+    {
+        [Test]
+        public void CallUserService_GetMinInfoMethod_AndFillViewModel_WhenPageIsLoad()
+        {
+            // Arrange
+            var mockedUserInfo = new UserDTO() { FirstName = "Pesho", LastName = "Peshev", Username = "pesho", ImageUrl = "image" };
+            var mockedImageService = new Mock<IImageUploadService>();
+            var mockedView = new Mock<IProfileView>();
+            mockedView.Setup(v => v.Model).Returns(new ProfileViewModel());
+
+            var mockedUserService = new Mock<IUserService>();
+            mockedUserService.Setup(s => s.GetMinInfo(It.IsAny<string>())).Returns(mockedUserInfo);
+
+            var presenter = new ProfilePresenter(mockedView.Object, mockedUserService.Object, mockedImageService.Object);
+            var ev = new IdEventArgs(null);
+
+            // Act
+            mockedView.Raise(v => v.PageLoad += null, ev);
+
+            // Assert
+            Assert.AreEqual($"{mockedUserInfo.FirstName}  {mockedUserInfo.LastName}", mockedView.Object.Model.Names);
+            Assert.AreEqual(mockedUserInfo.Username, mockedView.Object.Model.Username);
+            Assert.AreEqual(mockedUserInfo.ImageUrl, mockedView.Object.Model.ImageUrl);
+        }
+    }
+}
